9528b44064f1f94ab914a15ab4a7bb1821bb21c2
Jed Fox <git@jedfox.com>
2022-05-17 17:05:01 -0400

    does everything I want *except* single char transpose

--- src/extension.ts
+++ src/extension.ts
@@ -2,23 +2,101 @@
 
 import * as vscode from "vscode";
 
+const ops = [
+  "+",
+  "-",
+  "*",
+  "/",
+  "%",
+  "^",
+  "&",
+  "|",
+  "<<",
+  ">>",
+  ">>>",
+  "==",
+  "!=",
+  "===",
+  "!==",
+  "<",
+  ">",
+  "<=",
+  ">=",
+  "&&",
+  "||",
+  "=",
+];
+
+const delims = [",", ";", ":"];
+
+const separators = [...ops, ...delims];
+
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
-    "extension.reverseWord",
+    "extension.transpose",
     function () {
-      // Get the active text editor
       const editor = vscode.window.activeTextEditor;
 
-      if (editor) {
-        const document = editor.document;
-        const selection = editor.selection;
+      if (!editor) return;
 
-        // Get the word within the selection
-        const word = document.getText(selection);
-        const reversed = word.split("").reverse().join("");
-        editor.edit((editBuilder) => {
-          editBuilder.replace(selection, reversed);
-        });
+      const { document, selection } = editor;
+
+      const toTranspose = document.getText(selection);
+
+      let transposed = "";
+      let confused = false;
+
+      const queue = [...separators];
+      while (queue.length > 0) {
+        const sep = queue.shift()!;
+        const splits = toTranspose.split(sep);
+        if (splits.length === 2) {
+          console.log(`hit '${sep}'`);
+          if (transposed) {
+            confused = true;
+            break;
+          }
+
+          const [a, b] = splits;
+          transposed = `${b}${sep}${a}`;
+          if (
+            delims.includes(sep) &&
+            toTranspose.split(sep + " ").length === 2
+          ) {
+            console.log(`adding space to ${sep}`);
+            transposed = "";
+
+            queue.unshift(sep + " ");
+          } else if (
+            ops.includes(sep) &&
+            toTranspose.split(` ${sep} `).length === 2
+          ) {
+            console.log(`adding spaces around ${sep}`);
+            transposed = "";
+
+            queue.unshift(` ${sep} `);
+          }
+        }
+      }
+
+      if (!transposed) {
+        const splits = toTranspose.split(" ");
+        if (splits.length === 2) {
+          const [a, b] = splits;
+          transposed = `${b} ${a}`;
+        }
+      }
+
+      if (transposed) {
+        if (confused) {
+          vscode.window.showErrorMessage("Cannot transpose this selection");
+        } else {
+          editor.edit((editBuilder) => {
+            editBuilder.replace(selection, transposed);
+          });
+        }
+      } else {
+        vscode.window.showErrorMessage("Cannot transpose this selection");
       }
     }
   );
